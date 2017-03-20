adafb1b163affcc2f375aef09af62a383812940c
Diogo Carvalho <diogo.jmmc@gmail.com>
2017-03-20 17:49:53 +0000

    Guardar alterações

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,15 +1,9 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  
-  def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
-  end
-  
   test "should get new" do
-    get signup_path
+    get users_new_url
     assert_response :success
-    assert_select "title", "Sign Up | #{@base_title}"
   end
 
 end
