f8646a5a0d19512bf5a2e9ab9ca652045515f707
Diogo Carvalho <diogo.jmmc@gmail.com>
2017-03-30 13:24:19 +0000

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  attr_accessor :activation_token
+  attr_accessor :activation_token, :reset_token
   before_save :downcase_email 
   before_create :create_activation_digest
   
@@ -12,21 +12,21 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   
-  #Returns true if given token matches the digest
+  # Returns true if given token matches the digest
   def authenticated?(attribute, token)
     digest = send("#{attribute}_digest")
     return false if digest.nil?
     BCrypt::Password.new(digest).is_password?(token)
   end
   
-  #Returns the hash digest of the given string.
+  # Returns the hash digest of the given string.
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
   
-  #Returns a random token
+  # Returns a random token
   def User.new_token
     SecureRandom.urlsafe_base64
   end
@@ -39,6 +39,24 @@ class User < ApplicationRecord
   # Sends activation email.
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+  
+  # Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
+    #update_attribute(:reset_digest, User.digest(reset_token))
+    #update_attribute(:reset_sent_at, Time.zone.now)
+  end
+  
+  # Sends password reset email
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+  
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
   
   private
